665465e6e75906ec1f21620c9a6e3da89f641379
sayan <sayansinha5@gmail.com>
2022-05-13 08:18:11 +0530

    :sparkles: Tax Benefits Form

---
+++ HitayaApp/src/app/tax-benefit-form/tax-benefit-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tax-benefit-form',
+  templateUrl: './tax-benefit-form.component.html',
+  styleUrls: ['./tax-benefit-form.component.scss']
+})
+export class TaxBenefitFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
