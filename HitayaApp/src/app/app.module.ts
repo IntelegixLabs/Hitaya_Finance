6b21a75eb554f0481d6bba9e7cc45ab2ef79e186
ARNAB DAS <raj713335@gmail.com>
2022-05-14 06:07:32 +0530

    :wrench: Added signup functionality

--- HitayaApp/src/app/app.module.ts
+++ HitayaApp/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './loading/loading.component';
@@ -44,7 +47,10 @@ import { UserLayoutComponent } from './user-layout/user-layout.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
