e09f02bf0b738b1e260e8e300fe3f9a6854af5ad
ARNAB DAS <raj713335@gmail.com>
2022-05-14 05:38:03 +0530

    :wrench: updated the registration component

--- HitayaApp/src/app/registration/registration.component.ts
+++ HitayaApp/src/app/registration/registration.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { IUserDetail } from '../tax-interface/IUserDetail';
+
+import { UserValidationService } from '../tax-services/user-validation/user-validation.service';
+
 
 @Component({
   selector: 'app-registration',
@@ -9,27 +15,41 @@ import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  siteKey: string;
+  status: number;
+  errMsg: string;
+  msg: string;
+  credentials: IUserDetail;
+
+  constructor(private login: UserValidationService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSelectFile(event) { 
-    console.log("OnSelect File");
+  SignUp(form: NgForm) {
+
+    console.log("registeration-form");
+
+    this.credentials = { PAN_CARD: form.value.PanCard, INDIVIDUAL_TAX: form.value.IndividulaTax, FIRSTNAME: form.value.FirstName, LASTNAME: form.value.LastName, PHONE_NUMBER: form.value.PhoneNumber, EMAILID: form.value.Email, GENDER: form.value.Gender, DOB: form.value.DOB, ADDRESS: form.value.Address, PIN: form.value.Pin, RESIDENT: form.value.Resident, PASSWORD: form.value.Password };
+
+    console.log(this.credentials);
+
+    this.login.signup(this.credentials).subscribe(
+      responseLoginStatus => {
+        this.status = responseLoginStatus;
+        if (this.status == 1) {
+          sessionStorage.setItem('userName', form.value.PanCard);
+          sessionStorage.setItem('userRole', 'User');
+          this.router.navigate(['/index']);
+        }
+        else {
+          this.msg = this.status + ". Try again with valid credentials.";
+          alert("invalid");
+        }
+      },
+      () => console.log("SignUp Successfull")
+    );
   }
 
- 
-  getAccountAndBalance = () => {
-    console.log("Get Account Balance");
-  }
-
-
-  SubmitSignUpForm(form: NgForm) {
-    console.log("SubmitSignUpForm");
-  }
-
-
-  submitLoginForm(form: NgForm) {
-    console.log("SubmitLoginForm");
-  }
+  
 }
